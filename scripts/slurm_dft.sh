87f56ee1f7143735c4056520cbccf21555f51ba0
jushinpon <jushinpon@gmail>
2022-06-05 02:48:59 +0800

    initial commit

---
+++ scripts/slurm_dft.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#sed_anchor01
+#SBATCH --output=dp01.out
+#SBATCH --job-name=dp01
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=8
+#SBATCH --partition=debug
+##SBATCH --exclude=node18,node20
+export LD_LIBRARY_PATH=/opt/mpich-3.4.2/lib:$LD_LIBRARY_PATH
+export PATH=/opt/mpich-3.4.2/bin:$PATH
+export LD_LIBRARY_PATH=/opt/intel/compilers_and_libraries_2018.0.128/linux/mkl/lib/intel64_lin:$LD_LIBRARY_PATH
+#export PATH=/opt/mpich-3.4.2/bin:$PATH
+#mpiexec_anchor
+mpiexec /opt/QEGCC_MPICH3.4.2/bin/pw.x -in dft_script.in
+echo "Done" > dft_done.txt
